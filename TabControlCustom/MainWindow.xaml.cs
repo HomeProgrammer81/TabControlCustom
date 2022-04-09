d3ea92e8930a1b2026a53c6f88f62d848f9d0e92
Hayato.Matsumuro@gmail.com <Hayato.Matsumuro@gmail.com>
2022-04-09 09:05:42 +0900

    追加ボタン付タブコントロールの作成

--- TabControlCustom/MainWindow.xaml.cs
+++ TabControlCustom/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace TabControlCustom
         public MainWindow()
         {
             InitializeComponent();
+
+            // 先頭のタブの設定
+            TabItem item = (TabItem)TabControlAddDel.Items.GetItemAt(0);
+            item.Header = "タブ1";
+            item.Content = new TabItemUserControl(1);
+        }
+
+        private void SelectedAddDelTab(object sender, SelectionChangedEventArgs e)
+        {
+            // 選択したタブのインデックス
+            int idx = TabControlAddDel.SelectedIndex;
+
+            // タブの総数
+            int count = TabControlAddDel.Items.Count;
+
+            // 追加ボタン(＋)を選択する
+            if (idx == count - 1)
+            {
+                // 追加するタブの設定
+                TabItem item = new TabItem();
+                item.Header = "タブ" + count;
+                item.Content = new TabItemUserControl(count);
+
+                // 追加ボタンの前にタブを追加
+                TabControlAddDel.Items.Insert(count - 1, item);
+
+                // 追加したタブを選択状態にする
+                TabControlAddDel.SelectedIndex = count - 1;
+            }
         }
     }
 }
