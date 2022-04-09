6787b210fff07ecf87b2e2764549c1afa79b2021
Hayato.Matsumuro@gmail.com <Hayato.Matsumuro@gmail.com>
2022-04-09 17:19:24 +0900

    タブ削除ボタン追加

---
+++ TabControlCustom/TabHeaderUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TabControlCustom
+{
+    /// <summary>
+    /// TabHeaderUserControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TabHeaderUserControl : UserControl
+    {
+        public TabHeaderUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public TabHeaderUserControl(int num)
+        {
+            InitializeComponent();
+
+            label.Content = "タブ" + num;
+        }
+
+        private void ClickedDelete(object sender, RoutedEventArgs e)
+        {
+            TabItem item = (TabItem)this.Parent;
+            TabControl tabControl = (TabControl)item.Parent;
+
+            // 1つ前のタブを選択する
+            tabControl.SelectedIndex--;
+
+            // タブを削除する
+            tabControl.Items.Remove(item);
+        }
+    }
+}
